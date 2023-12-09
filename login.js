a9ecabd1057e83f1da8e1cc8f085b84e55439bd9
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-12-09 12:19:57 +0500

    working on alert when user not register him self

--- login.js
+++ login.js
@@ -15,6 +15,25 @@ const login = () => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log("errorMessage==>", errorMessage);
+        if(errorMessage === "Firebase: Error (auth/invalid-credential)."){
+          Swal.fire({
+            title: "Register Now" + "<br/> " + "Do you want to Register Now?",
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: "Register",
+            denyButtonText: `Don't Register`
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              Swal.fire("Thank you!", "", "success");
+              setInterval(()=>{
+                window.location.href = "Register.html"
+              }, 10)
+            } else if (result.isDenied) {
+              Swal.fire("Not registered", "", "Register to explore more");
+            }
+          });
+        }
       });
   };
   document.getElementById("loginBtn").addEventListener("click", login);
