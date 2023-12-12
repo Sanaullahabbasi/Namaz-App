c280bd56b4edeaddfd5e499ea54ceb2366c14a00
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-12-12 23:20:49 +0500

    updated

--- otp.js
+++ otp.js
@@ -1,26 +1,40 @@
 import { auth, RecaptchaVerifier, signInWithPhoneNumber } from "./firebase.js";
 
-window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {});
+let confirmation;
 let submitOTP = document.getElementById("submitOTP");
+let registerWithPhone = document.getElementById("registerWithPhone");
 
-const phoneVerify = ()=>{
-    const appVerifier = window.recaptchaVerifier;
+const registerd = ()=>{
     const phoneNumber = document.getElementById("phone");
-    console.log(phoneNumber)
-
+    window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {});
+    const appVerifier = window.recaptchaVerifier;
+    console.log("phoneNumber-->", phoneNumber.value)
 
     
     signInWithPhoneNumber(auth, `+${phoneNumber.value}`, appVerifier)
         .then((confirmationResult) => {
-            console.log("sms sent")
-
-          window.confirmationResult = confirmationResult;
+            confirmation = confirmationResult
+            console.log("sms sent", confirmation)
      
         }).catch((error) => {
             console.log("sms not sent")
           
         });
 }
+
+const phoneVerify = ()=>{
+    let otp = document.getElementById("otp");
+    confirmation.confirm(otp.value).then((result) => {
+        const user = result.user;
+        console.log("user registered -->", user)
+ 
+      }).catch((error) => {
+       console.log("user not registered-->", error);
+      });
+}
 if(submitOTP){
     submitOTP.addEventListener("click", phoneVerify)
+}
+if(registerWithPhone){
+    registerWithPhone.addEventListener("click", registerd)
 }
